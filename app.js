8d6f27d9e724ea91c56d7e853d11513e8cbf688a
Joseph <josephmhansen@gmail.com>
2017-03-02 12:36:30 -0700

    added semicolons, is Angular minify-ing?

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 angular.module('BuyBeasts', [])
-.controller('mainCtrl', mainCtrl)
+.controller('mainCtrl', mainCtrl);
 //.directive('beastDirective', beastDirective)
 
 function mainCtrl($scope) {
@@ -7,9 +7,9 @@ function mainCtrl($scope) {
         
     //$scope.beasts.push({name: 'Demiguise', price: 5000, quantity: 2});
     //$scope.beasts.push({name: 'Bowtruckle', price: 3500, quantity: 10});
-    $scope.beasts.push(new Demiguise())
-    $scope.beasts.push(new Bowtruckle())
-    $scope.beasts.push(new Erumpent())
+    $scope.beasts.push(new Demiguise());
+    $scope.beasts.push(new Bowtruckle());
+    $scope.beasts.push(new Erumpent());
 }
 
 /*function beastDirective() {
--- beasts.js
+++ beasts.js
@@ -3,26 +3,26 @@
 
 class BeastInventoryItem {
     constructor(name, price, quantity) {
-        this.name = name
-        this.price = price
-        this.quantityInStock = quantity
+        this.name = name;
+        this.price = price;
+        this.quantityInStock = quantity;
     }
 }
 
 class Demiguise extends BeastInventoryItem {
     constructor() {
-        super('Demiguise', 5000, 2)
+        super('Demiguise', 5000, 2);
     }
 }
 
 class Bowtruckle extends BeastInventoryItem {
     constructor() {
-        super('Bowtruckle', 3500, 10)
+        super('Bowtruckle', 3500, 10);
     }
 }
 
 class Erumpent extends BeastInventoryItem {
     constructor() {
-        super('Erumpent', 4000, 0)
+        super('Erumpent', 4000, 0);
     }
 }
