76c863ba21fa8a80d19620e801fda4c377cc63d8
Joseph Hansen <josephmhansen@gmail.com>
2017-03-02 21:27:41 +0000

    moved beasts.js into app.js

--- app.js
+++ app.js
@@ -1,3 +1,32 @@
+// Finding list here: https://www.dailydot.com/parsec/list-of-fantastic-beasts/
+// and here: http://harrypotter.wikia.com/wiki/Fantastic_Beasts_and_Where_to_Find_Them
+
+class BeastInventoryItem {
+    constructor(name, price, quantity) {
+        this.name = name;
+        this.price = price;
+        this.quantity = quantity;
+    }
+}
+
+class Demiguise extends BeastInventoryItem {
+    constructor() {
+        super('Demiguise', 5000, 2);
+    }
+}
+
+class Bowtruckle extends BeastInventoryItem {
+    constructor() {
+        super('Bowtruckle', 3500, 10);
+    }
+}
+
+class Erumpent extends BeastInventoryItem {
+    constructor() {
+        super('Erumpent', 4000, 0);
+    }
+}
+
 angular.module('BuyBeasts', ['ui.router'])
 .config([
     '$stateProvider',
